5d1eaa0fad3e9fc755c44d6cef307ccd70118d0c
Bryton Kilonzo <bryton90@users.noreply.github.com>
2025-10-30 17:15:48 +0300

    chore: Update configurations

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // Base Next.js configuration
-  reactStrictMode: true,
-  swcMinify: true,
+  
   // Add any custom Next.js configuration here
 };
 
